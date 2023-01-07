4b3da828e4f289ec856a832f5dae3e150ab50319
Yann Klein <yann.klein@me.com>
2023-01-07 10:59:26 +0900

    lecture done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -9,9 +8,15 @@ Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
 
   # See all restaurants
-  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants', to: 'restaurants#index', as: 'restaurants'
   # See details about one restaurant
+  get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
   # Create a restaurant
+  post 'restaurants', to: 'restaurants#create'
   # Update a restaurant
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+  patch 'restaurants/:id', to: 'restaurants#update'
   # Destroy a restaurant
+  delete 'restaurants/:id', to: "restaurants#destroy"
 end
